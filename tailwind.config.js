a4b3d0bab6356c00d653358c5fb1010c6e8836f0
Nebelung <nebelung@mailfence.com>
2024-08-30 04:42:23 +0000

    Server, Credits, and Introduction

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,34 @@ export default {
 			},
 		},
 		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						"code::before": { content: "none" },
+						"code::after": { content: "none" },
+						code: {
+							backgroundColor: "#24292e",
+							color: "#e1e4e8",
+							padding: "0.2em 0.4em",
+							borderRadius: "6px",
+						},
+					},
+				},
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
